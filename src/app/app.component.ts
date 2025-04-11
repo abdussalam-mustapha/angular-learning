5ec27a0321885708530499756a8735249f53e033
abdussalam-mustapha <abdussalammustapha07@gmail.com>
2025-04-11 08:06:20 +0100

    pushed first angular code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from "@angular/forms"
+import { NgClass, NgFor } from '@angular/common';
+
+
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgFor, NgClass],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-learninf';
+  todoList : TodoItem [] = []
+  newTask: string = ''
+
+  addTask():void{
+    if (this.newTask.trim() !== '') {
+      const newTodoItem: TodoItem = {
+        id: Date.now(),
+        task: this.newTask,
+        completed: false
+      }
+
+      this.todoList.push(newTodoItem)
+      this.newTask = ''
+    }
+  }
+
+  toggleComplete(index: number): void{
+    this.todoList[index].completed = !this.todoList[index].completed
+  }
+
+  deleteTask(id: number): void {
+    this.todoList = this.todoList.filter(item => item.id !== id)
+  }
 }
